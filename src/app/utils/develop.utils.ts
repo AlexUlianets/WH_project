5a547264d0aa505f105d288b92698c31b59bb560
Sasha15 NA <sashau087@gmail.com>
2018-04-05 12:16:21 +0300

    Fixes

--- src/app/utils/develop.utils.ts
+++ src/app/utils/develop.utils.ts
@@ -1,14 +1,17 @@
 export class DevelopUtil {
 
   /*private static HOST = 'http://weathermap.tk/';*/
+  /*private static HOST = 'http://35.173.37.26:8888/';*/
+  /*private static HOST = 'http://localhost:5000/';*/
 
-  private static HOST = 'http://35.173.37.26:8888/';
+  private static HOST = 'https://map.worldweatheronline.com:8888/';
 
   public static getHost() {
     return DevelopUtil.HOST;
   }
 
   public static url(url: string) {
+    console.log('host: ' + DevelopUtil.HOST);
     return DevelopUtil.HOST + url;
   }
 }
